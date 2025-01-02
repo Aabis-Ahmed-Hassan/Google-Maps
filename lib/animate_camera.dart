47a928fa28448acab1b2b27aeee85090f0ee01ba
Aabis-Ahmed-Hassan <aabisahmedhassan3@gmail.com>
2025-01-02 11:10:11 +0500

    Done with Animate Camera

---
+++ lib/animate_camera.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class AnimateCamera extends StatefulWidget {
+  const AnimateCamera({super.key});
+
+  @override
+  State<AnimateCamera> createState() => _AnimateCameraState();
+}
+
+class _AnimateCameraState extends State<AnimateCamera> {
+  Completer<GoogleMapController> _mapController = Completer();
+  CameraPosition _cameraPosition =
+      CameraPosition(target: LatLng(23.123, 21.234), zoom: 5);
+
+  List<Marker> markers = [
+    Marker(
+      markerId: MarkerId('Initial Camera Position'),
+      position: LatLng(23.123, 21.234),
+      infoWindow: InfoWindow(title: 'Initial Camera Position'),
+    ),
+    Marker(
+      markerId: MarkerId('China'),
+      infoWindow: InfoWindow(
+        title: 'China',
+      ),
+      position: LatLng(38.7946, 106.5348),
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: _cameraPosition,
+        markers: Set.of(markers),
+        onMapCreated: (GoogleMapController controller) {
+          _mapController.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          GoogleMapController controller = await _mapController.future;
+
+          controller.animateCamera(
+            CameraUpdate.newCameraPosition(
+              CameraPosition(
+                target: LatLng(38.7946, 106.5348),
+              ),
+            ),
+          );
+        },
+        child: Icon(
+          Icons.room_preferences_sharp,
+        ),
+      ),
+    );
+  }
+}
